ebc8024d72ab00260f5ca1e4a9bc5757f6036df9
Sachin Bhola <sbsachin699@gmail.com>
2020-04-26 12:52:46 +0530

    fixed build fail issue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  mailsystembaseapiurl: 'http://176.9.137.77:8005',
-  apibase: 'http://176.9.137.77:3000',
+  mailsystembaseapiurl: 'http://176.9.137.77:8012',
+  apibase: 'http://176.9.137.77:3001',
 };
